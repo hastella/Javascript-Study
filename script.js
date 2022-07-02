5ca106808cf9ba46120445368fd497eb16391b10
Stella_Ha <sangyeeh@gmail.com>
2022-07-02 22:08:09 +0900

    fourth commit

--- script.js
+++ script.js
@@ -166,3 +166,75 @@ let student = {
   console.log("첫번째 학생은:", studentLst[0])
   console.log("첫번째 학생의 나이는?", studentLst[0].age) //또는
   console.log("첫번째 학생의 나이는?", studentLst[0]["age"])
+// end of Object //
+
+// if문 //
+//문제 1: 유저가 입력하는 숫자가 0인지 음성인지 양성인지 판단하는 프로그램을 만드시오
+let number = -1
+
+if(number > 0) {
+  console.log("양성")
+} else if (number < 0) {
+  console.log("음성")
+} else {
+  console.log("0")
+}
+//레포트 점수에따라 등급을 매기는 프로그램을 만드시오
+let score = 69
+
+//나의 답
+if (score >= 90 && score <= 100) {
+  console.log("A")
+} else if (score >= 80 && score <= 89) {
+  console.log("B")
+} else if (score >= 70 && score <= 79) {
+  console.log("C")
+} else if (score >= 60 && score <= 69) {
+  console.log("D")
+} else {
+  console.log("F")
+}
+
+//답
+let score =  90
+let grade =''
+if(90<=score && score<=100){
+  grade = "A"
+}else if(80<=score && score<=89){
+  grade = "B"
+}else if(70<=score && score<=79){
+  grade ="C"
+}else if(60<=score && score<=69){
+  grade="D"
+}else if(0<=score && score<=59){
+  grade="F"
+}else {
+  console.log("잘못된 범위의 점수입니다")
+}
+console.log(grade)
+
+//한 지원자가 우리회사에 지원을했다. 지원자가 사용가능한 스킬은 배열에 제공이 된다
+let skills = ["HTML","CSS","Javascript","React"]
+
+  //Javascript와 React 둘다 할줄 안다면 “합격!” Javascript와 React 둘중 하나만 할줄 안다면 “예비”, 두 스킬이 없다면 “탈락” 을 보여주는 프로그램을 짜시오 //
+
+//나의 답
+if (skills=="Javascript" && "React") {
+  console.log("합격!")
+} else if (skills=="Javascript"||"React"){
+  console.log("예비")
+} else
+  console.log("탈락")
+
+let skills = ["HTML","CSS","Javascript","React"]
+
+//답
+if(skills.includes("Javascript") && skills.includes("React")){
+  console.log("합격!")
+}else if(skills.includes("Javascript") || skills.includes("React")){
+  console.log("예비")
+}else {
+  console.log("탈락!")
+}
+// end of if문 //
+
