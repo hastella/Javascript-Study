fbbe7cfcfc745239a9b3ebc1d183968c0271ce3a
Stella_Ha <sangyeeh@gmail.com>
2022-07-06 10:26:25 +0900

    function commit

--- script.js
+++ script.js
@@ -396,4 +396,158 @@ function serveCoke(){
 let result = makeBurger("불고기","L",3) //매개변수
 console.log("버거 프로세스 결과",result)
 
+  //function 을 이용한 문제들//
+    //Q1. "안녕 내 이름은 스텔라야"라는 문장을 프린트하는 함수 'greet’를 만드시오
+      function greet() {
+        console.log("안녕 내 이름은 스텔라야")
+      } 
+
+    //Q2. 이름을 매개변수로 전달받아 다양한 이름을 프린트 할 수 있게 함수를 수정하시오 예) “안녕 내 이름은 에밀리야”, “안녕 내 이름은 할리야”
+      function greet(x) {
+        console.log("안녕 내 이름은", x,"야")
+      } 
+
+    //Q3. 매개변수로 전달받은 이름을 반환하게 함수를 수정하시오
+      function greet(x) {
+        console.log("안녕 내 이름은", x,"야")
+        return x
+      }
+      //결과 확인법
+      const x = greet("스텔라")
+      console.log("안녕 내 이름은", x, "야")
+
+    //Q4. meetAt 함수를 만들어주세요.
+      //  인자를 세개 받습니다.
+
+      //  첫번째 인자는 년도에 해당하는 숫자입니다.
+      //  두번째 인자는 월에 해당하는 숫자입니다.
+
+      //  세번째 인자는 일에 해당하는 숫자입니다.
+
+      //  년도 인자만 받았을 경우 → "1234년" 과 같은 형식의 문자열을 리턴 해주세요.
+
+      //  년도,월 인자를 받았을 경우 → 년도와 월을 조합해서 "1234년 5월" 과 같은 형식의 문자열을 리턴 해주세요.
+
+      //  년도,월,일 인자를 전부 받았을 경우 → 년도,월,일을 조합해서 "1234/5/6" 과 같은 형식의 문자열을 리턴 해주세요.
+
+      // 결과 예시
+      // meetAt(2022); // 결과 --> 2022년
+
+      // meetAt(2032, 3); // 결과 --> "2032년 3월"
+      // meetAt(1987, 10, 28); // 결과 --> "1987/10/28"
+
+      //내가 시도한 방법
+      function meetAt(year, month, date) {
+
+        if (year, month, date) {
+          console.log(year,"/",month,"/",date)
+        } else if (year, month) {
+          console.log(year,"년 ",month,"월")
+        } else if (year) {
+          console.log(year,"년")
+        }
+      }
+
+      console.log(meetAt(2022,5))
+    
+      //답
+      function meetAt(year, month, date) {
+      
+        if (date){
+          return `${year}/${month}/${date}`
+        } else if(month) {
+          return `${year}년 ${month}월`
+        } else if(year) {
+          return `${year}년`
+        }
+      } 
+      
+      console.log(meetAt(2022,1,7))
+
+    //Q5. findSmallestElement 함수를 구현해 주세요.
+
+      // findSmallestElement 의 arr 인자는 숫자 값으로만 이루어진 배열입니다.
+
+      // arr 의 값들 중 가장 작은 값을 리턴 해주세요.
+
+      // 만일 arr 가 비어있으면 0을 리턴 해주세요.
+
+      // 예를 들어, 다음과 같은 배열이 인자(input)으로 들어왔다면 0이 리턴 되어야 합니다.
+      // 이용되는 배열
+      // [100,200,3,0,2,1]
+
+      //내가 시도한 방법 (Math.min.apply(null, arr)을 이용함)
+        function findSmallestElement(numArr) {
+    
+          let numMin = Math.min.apply(null, numArr); //array에서 가장 작은 수를 호출하는 식
+
+          if (numArr.length == 0) { //array가 비어있다 -> length가 0이다.
+            return 0
+          } else if (numArr) {
+            return numMin
+          }
+        }
+        
+        console.log(findSmallestElement([1,0,70,100]))
+
+      //답
+      function findSmallestElement(numArr) {
+  
+        if (numArr.length == 0) { //array가 비어있다 -> length가 0이다.
+          return 0
+        } 
+        let numMin = numArr[0]
+        for(let i=1; i<numArr.length; i++){
+          if (numMin > numArr[i]){
+              numMin = numArr[i]
+          }
+        }
+        return numMin
+      }
+      
+      console.log(findSmallestElement([]))
+
+    //Q6. 돈을 매개변수로 받으면 몇개의 지폐와 동전이 필요한지 최소 개수를 계산해주는 함수를 만드시오
+
+      // 출력예 )
+
+      // 12300인 경우
+      //  50000 X 0
+      //  10000 X 1
+      //  5000  X 0
+      //  1000 X 2
+      //  500 X 0
+      //  100 X 3
+
+      //내가 시도한 답이지만 완벽히 구현하지 못함 -> 반복문에서의 값을 다시 새로 업데이트 해주는 부분을 구현하지 못함!
+        function countMoney(amount){
+          let valueArr = [50000,10000,5000,1000,500,100]
+          let count = amount/valueArr[0]
+
+          for(let i=0; i<valueArr.length; i++){
+            if (amount%valueArr[i]==0){
+              return `${valueArr[i]} X ${count}`
+            }
+          }
+        }
+
+        console.log(countMoney(100000))
+
+      //답
+      let unit = [50000,10000,5000,1000,500,100]
+
+      function countMoney(amount){
+
+        for(let i=0; i<unit.length; i++){
+          let count = Math.floor(amount/unit[i]) //Math.floor
+          amount = amount - (unit[i] * count) //하고 난 후의 값을 다시 새로이 업데이트 해주는 부분
+          console.log(`${unit[i]} X ${count}`)
+        }
+      }
+      
+      console.log(countMoney(106000))
+
+
+
+   
 
